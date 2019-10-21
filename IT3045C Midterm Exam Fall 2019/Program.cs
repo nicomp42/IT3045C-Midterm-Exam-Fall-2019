660358725906c0f2fc4949384225eef1cd608724
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-10-21 11:33:21 -0400

    Add project files.

---
+++ IT3045C Midterm Exam Fall 2019/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IT3045C_Midterm_Exam_Fall_2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
