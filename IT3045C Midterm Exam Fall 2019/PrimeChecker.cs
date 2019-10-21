7eef27972a519902a40158be552014b7f06ed9cc
Bill Nicholson <nicholdw@ucmail.uc.edu>
2019-10-21 11:54:56 -0400

    First Commit

---
+++ IT3045C Midterm Exam Fall 2019/PrimeChecker.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * Solution to Midterm Exam IT3045C Fall 2019
+ */ 
+using System;
+
+namespace IT3045C_Midterm_Exam_Fall_2019
+{
+    class PrimeChecker
+    {
+        public static Boolean IsPrime(int num)
+        {
+            Boolean isPrime = true;     // Assume the best
+            int limit = num / 2;
+            if (num % 2 == 0) { return false; }
+            for (int i = 3; i <= limit; i++)
+            {
+                if (num % i == 0) { isPrime = false; break; }
+            }
+            return isPrime;
+        }
+        /// <summary>
+        /// Get the next higher or lower prime
+        /// </summary>
+        /// <param name="num">Number to start from</param>
+        /// <param name="increment">1 for next highest, -1 for next lowest</param>
+        /// <returns></returns>
+        public static int GetAdjacentPrime(int num, int increment)
+        {
+            int nextPrime = num + increment;
+            while (!IsPrime(nextPrime))
+            {
+                nextPrime += increment;
+            }
+            return nextPrime;
+        }
+    }
+}
--- IT3045C Midterm Exam Fall 2019/Program.cs
+++ IT3045C Midterm Exam Fall 2019/Program.cs
@@ -1,8 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Bill Nicholson
+ * nicholdw@ucmail.uc.edu
+ * Solution to Midterm Exam IT3045C Fall 2019
+ */
+using System;
 
 namespace IT3045C_Midterm_Exam_Fall_2019
 {
@@ -10,6 +11,18 @@ namespace IT3045C_Midterm_Exam_Fall_2019
     {
         static void Main(string[] args)
         {
+            int input = 0;
+            Console.WriteLine("Enter a positive integer please. ");
+            input = Convert.ToInt32(Console.ReadLine());
+            if (PrimeChecker.IsPrime(input))
+            {
+                Console.WriteLine("Next prime = " + PrimeChecker.GetAdjacentPrime(input, 1));
+                Console.WriteLine("Previous prime = " + PrimeChecker.GetAdjacentPrime(input, -1));
+
+            } else
+            {
+                Console.WriteLine(input + " is not prime");
+            }
         }
     }
 }
